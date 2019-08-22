a8631c9134792bc51f7fd98ea83d85237d113f71
tanlianwang <tanlianwang@jimimax.com>
2019-08-22 11:48:51 +0800

    update .gitignore

--- src/main/java/com/dlnu/byname/ByNameApplication.java
+++ src/main/java/com/dlnu/byname/ByNameApplication.java
@@ -7,7 +7,6 @@ import org.slf4j.LoggerFactory;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.context.annotation.Bean;
-
 import java.util.Properties;
 /**
  * 启动类
