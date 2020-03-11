4063f558030e87038ff2a24e428b0e4f54667ce1
anyant <1075379406@qq.com>
2020-03-11 20:39:27 +0800

    【增加】输入字符串自动去重

--- src/main/java/com/dlnu/byname/ByNameApplication.java
+++ src/main/java/com/dlnu/byname/ByNameApplication.java
@@ -1,13 +1,10 @@
 package com.dlnu.byname;
 
-import com.github.pagehelper.PageHelper;
 import org.mybatis.spring.annotation.MapperScan;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
-import org.springframework.context.annotation.Bean;
-import java.util.Properties;
 /**
  * 启动类
  * @param
