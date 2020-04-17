230d4cc9c82346c590b363eb5134b77e4e995895
Administrator <1075379406@qq.com>
2020-04-17 10:52:14 +0800

    [feat] 增加MQ监听

---
+++ src/main/java/com/dlnu/byname/common/ActiveMqConfig.java
@@ -0,0 +1,89 @@
+/*
+ * COPYRIGHT. ChongQing UTECH Technology Co., Ltd. 2020.
+ * ALL RIGHTS RESERVED.
+ *
+ * No part of this publication may be reproduced, stored in a retrieval system, or transmitted,
+ * on any form or by any means, electronic, mechanical, photocopying, recording,
+ * or otherwise, without the prior written permission of ChongQing UTECH Network Technology Co., Ltd.
+ *
+ * Amendment History:
+ *
+ * Date                   By              Description
+ * -------------------    -----------     -------------------------------------------
+ * 2020-04-14 15:31                  TanLianWang          Create the class
+ * http://www.chinautech.com/
+ */
+
+package com.dlnu.byname.common;
+
+import org.apache.activemq.ActiveMQConnectionFactory;
+import org.apache.activemq.command.ActiveMQQueue;
+import org.apache.activemq.command.ActiveMQTopic;
+import org.springframework.beans.factory.annotation.Value;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.jms.config.DefaultJmsListenerContainerFactory;
+import org.springframework.jms.config.JmsListenerContainerFactory;
+
+import javax.jms.Queue;
+import javax.jms.Topic;
+
+
+/**
+ * @author TanLianWang
+ * @version 1.0
+ * @date 2020/4/14 15:31
+ */
+//@Configuration
+public class ActiveMqConfig {
+    @Value("${spring.activemq.queueName}")
+    private String queueName;
+
+    @Value("${spring.activemq.topicName}")
+    private String topicName;
+
+    @Value("${spring.activemq.user}")
+    private String usrName;
+
+    @Value("${spring.activemq.password}")
+    private String password;
+
+    @Value("${spring.activemq.broker-url}")
+    private String brokerUrl;
+
+    @Bean
+    public Queue queue() {
+        //队列模式
+        return new ActiveMQQueue(queueName);
+    }
+
+    @Bean
+    public Topic topic() {
+        //发布订阅模式
+        return new ActiveMQTopic(topicName);
+    }
+
+    @Bean
+    public ActiveMQConnectionFactory connectionFactory() {
+        //创建连接
+        return new ActiveMQConnectionFactory(usrName, password, brokerUrl);
+    }
+
+    @Bean
+    public JmsListenerContainerFactory<?> jmsListenerContainerQueue(ActiveMQConnectionFactory connectionFactory) {
+        //队列监听
+        DefaultJmsListenerContainerFactory bean = new DefaultJmsListenerContainerFactory();
+        bean.setConnectionFactory(connectionFactory);
+        return bean;
+    }
+
+    @Bean
+    public JmsListenerContainerFactory<?> jmsListenerContainerTopic(ActiveMQConnectionFactory connectionFactory) {
+        //发布订阅监听
+        DefaultJmsListenerContainerFactory bean = new DefaultJmsListenerContainerFactory();
+        bean.setPubSubDomain(true);
+        bean.setConnectionFactory(connectionFactory);
+        return bean;
+    }
+
+}
