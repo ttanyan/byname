15a241daff6dc403824db39f7d452077709e898a
AnyanT <tanlianwang@qq.com>
2021-04-25 01:29:59 +0800

    [add]测试github提交

--- src/main/java/com/dlnu/byname/annotation/ParamVerify.java
+++ src/main/java/com/dlnu/byname/annotation/ParamVerify.java
@@ -8,6 +8,7 @@ import java.lang.annotation.*;
  * @version 1.0 
  * @date 2020/5/12 9:49
  */
+//TODO 自定义注解如何实现  2021-04-25
 @Documented
 @Target(ElementType.METHOD)
 @Retention(RetentionPolicy.RUNTIME)
